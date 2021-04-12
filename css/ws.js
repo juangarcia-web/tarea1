17b44638d805d391d28f53a89645561284e6b9ee
Your Name <juangarcia170593@gmail.com>
2021-04-12 18:17:16 -0500

    initial commit

---
+++ css/ws.js
@@ -0,0 +1,8 @@
+user = { name: 'Daniel', email: 'daniel@gmail.com', gender: 'Male' }
+return user.email;
+
+// function getEmail(user) {
+    
+// }
+
+// console.log(getEmail([{name: 'Daniel', email: 'daniel@gmail.com', gender: 'Male' }}]))
